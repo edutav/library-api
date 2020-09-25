1e2cb40303078f467c51295ba6cf93c3ae9b9b6b
Eduardo Tavares <edutav@gmail.com>
2020-09-25 08:25:25 -0400

    adicionado assinatura do método getById.

--- src/main/java/br/com/library/services/BookService.java
+++ src/main/java/br/com/library/services/BookService.java
@@ -2,6 +2,10 @@ package br.com.library.services;
 
 import br.com.library.model.entity.Book;
 
+import java.util.Optional;
+
 public interface BookService {
 	Book save(Book book);
+
+	Optional<Book> getById(Long id);
 }
