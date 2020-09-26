c6f59840def0d49b1f0d9a9f7d5c9499a36325e6
Eduardo Tavares <edutav@gmail.com>
2020-09-26 11:34:35 -0400

    adicionado teste de integração com o repository para obter um livro por id

--- src/test/java/br/com/library/model/repositories/BookRepositoryTest.java
+++ src/test/java/br/com/library/model/repositories/BookRepositoryTest.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
@@ -50,6 +52,20 @@ public class BookRepositoryTest {
 
 		//verificação
 		assertThat(exists).isFalse();
+	}
+
+	@Test
+	@DisplayName("Deve obter um livro por id.")
+	public void findByIdTest() {
+		//cenário
+		Book book = createBook();
+		entityManager.persist(book);
+
+		//execução
+		Optional<Book> foundBook = repository.findById(book.getId());
+
+		//verificação
+		assertThat(foundBook.isPresent()).isTrue();
 	}
 
 	private Book createBook() {
