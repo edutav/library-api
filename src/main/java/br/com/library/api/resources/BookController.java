3827dfb0a486ba5385b741d46fd7cec8b154d912
Eduardo Tavares <edutav@gmail.com>
2020-09-26 15:08:22 -0400

    adicionado método para realizar buscas

--- src/main/java/br/com/library/api/resources/BookController.java
+++ src/main/java/br/com/library/api/resources/BookController.java
@@ -6,6 +6,9 @@ import br.com.library.exceptions.BusinessException;
 import br.com.library.model.entity.Book;
 import br.com.library.services.BookService;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -21,8 +24,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.validation.Path;
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/books")
@@ -66,6 +70,18 @@ public class BookController {
 			book = bookService.update(book);
 			return modelMapper.map(book, BookDTO.class);
 		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+
+	@GetMapping
+	public Page<BookDTO> find(BookDTO dto, Pageable pageRequest) {
+		Book filter = modelMapper.map(dto, Book.class);
+		Page<Book> result = bookService.find(filter, pageRequest);
+		List<BookDTO> list = result.getContent()
+				.stream()
+				.map(entity -> modelMapper.map(entity, BookDTO.class))
+				.collect(Collectors.toList());
+
+		return new PageImpl<BookDTO>(list, pageRequest, result.getTotalElements());
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
