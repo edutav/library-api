c8f71e88ea7edca777de7c7027a3516d261d9c65
Eduardo Tavares <edutav@gmail.com>
2020-09-26 11:36:09 -0400

    adicionados métodos para deletar e atualizar livro.

--- src/main/java/br/com/library/api/resources/BookController.java
+++ src/main/java/br/com/library/api/resources/BookController.java
@@ -9,16 +9,19 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Path;
 import javax.validation.Valid;
 
 @RestController
@@ -46,6 +49,23 @@ public class BookController {
 	public BookDTO get(@PathVariable Long id) {
 		return bookService.getById(id).map(book -> modelMapper.map(book, BookDTO.class))
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		Book book = bookService.getById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		bookService.delete(book);
+	}
+
+	@PutMapping("{id}")
+	public BookDTO update(@PathVariable Long id, BookDTO dto) {
+		return bookService.getById(id).map(book -> {
+			book.setAuthor(dto.getAuthor());
+			book.setTitle(dto.getTitle());
+			book = bookService.update(book);
+			return modelMapper.map(book, BookDTO.class);
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
